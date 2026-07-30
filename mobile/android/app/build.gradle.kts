eb22e12b4620ec1bc87d0baf1831c21d54adb0ae
zjcrop <127176814+zjcrop@users.noreply.github.com>
2026-07-30 09:15:49 -0700

    Add Android 1.0 packaging: mobile/android/app/build.gradle.kts

---
+++ mobile/android/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.zjcrop.grindpsd"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.zjcrop.grindpsd"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
